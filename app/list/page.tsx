dfb043da8de1c06b905c339902d42a06740866dd
Hyeon Um <dja12356@gmail.com>
2024-07-08 22:48:11 +0900

    navbar

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,5 +3,15 @@ import React from "react";
 type Props = {};
 
 export default function List({}: Props) {
-  return <h4 className="title">상품목록</h4>;
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      <div className="food">
+        <h4>상품 1 $200</h4>
+      </div>
+      <div className="food">
+        <h4>상품 2 $200</h4>
+      </div>
+    </div>
+  );
 }
