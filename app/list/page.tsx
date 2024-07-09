f34da50e8708021e7f2786ba36189bfa4fa2d227
Hyeon Um <dja12356@gmail.com>
2024-07-09 09:51:46 +0900

    minor map function

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,15 +3,17 @@ import React from "react";
 type Props = {};
 
 export default function List({}: Props) {
+  let 상품 = ["Tomato", "Pasta", "Coconut"];
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      <div className="food">
-        <h4>상품 1 $200</h4>
-      </div>
-      <div className="food">
-        <h4>상품 2 $200</h4>
-      </div>
+      {상품.map((item, idx) => {
+        return (
+          <div key={idx} className="food">
+            <h4>{item}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
